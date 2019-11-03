3d4c5b4f8501e293720ad3aff84a2043a37e8ea3
Tobias Heide <slepzs@gmail.com>
2019-11-03 20:57:49 +0100

    First commit

---
+++ resources/views/layouts/admin/admin.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <title>Admin Area</title>
+
+    <link rel="stylesheet" href="{{asset('css/libs.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+
+
+
+</head>
+
+<body id="admin-login">
+
+
+<!-- Navigation -->
+<div class="top-nav">
+    <div class="left-side-nav">
+        <div class="profile-img">
+            <img src="/images/jackblack.jpg" alt="">
+            <p>{{ $user->name }}</p>
+            <span uk-icon="menu" uk-toggle="target: #offcanvas-reveal"></span>
+        </div>
+    </div>
+    <div class="right-side-nav">
+        <div class="site-links">
+            <ul>
+                <li><a href="/">LINK</a></li>
+                <li><a href="/">LINK</a></li>
+                <li><a href="/">LINK</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+</div>
+
+<div class="admin-content-wrapper">
+
+    <div id="offcanvas-reveal" uk-offcanvas="mode: reveal; overlay: true">
+        <div class="uk-offcanvas-bar sidenav">
+        <div class="sidenav-header">
+            <h2>Wine Club</h2>
+            <button id="sidebarnav-toggle" class="uk-offcanvas-close" uk-close></button>
+        </div>
+
+        <ul class="pages">
+            <a href="#"><li>Pages</li></a>
+            <a href="#"><li>Pages</li></a>
+            <a href="#"><li>Pages</li></a>
+            <a href="#"><li>Pages</li></a>
+        </ul>
+
+
+        </div>
+    </div>
+
+    <div class="content">
+        @yield('content')
+    </div>
+</div>
+
+
+<script src="{{asset('js/libs.js')}}"></script>
+<script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous">
+
+</script>
+<script type="text/javascript">
+
+
+
+</script>
+</body>
+
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,32 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+
+
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/admininterface/admin.scss', 'public/css/styles.css',);
+
+
+ mix.styles([
+    'resources/sass/framework/uikit.css'
+
+ ], 'public/css/libs.css');
+
+mix.scripts([
+    'resources/js/framework/uikit.js',
+    'resources/js/framework/uikit-icons.js'
+
+], 'public/js/libs.js');
+
+mix.browserSync('wineclub.test');
