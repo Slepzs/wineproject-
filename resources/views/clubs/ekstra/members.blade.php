1b976d6790ca48ef9cd0a815de821e11bb9af8f0
Tobias Heide <slepzs@gmail.com>
2019-12-12 12:29:13 +0100

    members error

--- resources/views/clubs/ekstra/members.blade.php
+++ resources/views/clubs/ekstra/members.blade.php
@@ -20,7 +20,5 @@
 
 
 @endforeach
-<div class="primary-btn primary-btn--center">
-    <a href="#">View more</a>
-</div>
+
 </div>
--- resources/views/clubs/show.blade.php
+++ resources/views/clubs/show.blade.php
@@ -87,43 +87,19 @@
 
                 <!-- CLUB MEMBERS -->
                 <div class="club-profile-members">
-                    <div class="uk-navbar-container" uk-navbar>
-                        <div class="uk-navbar-left">
-                            <a class="uk-navbar-toggle" uk-navbar-toggle-icon href=""></a>
-                        </div>
-                    </div>
 
 
 
 
                     <h2 class="uk-card-title">Club members</h2>
                     <span><a href="{{ route('clubManagement.users', $club->id) }}">(Edit club list)</a></span>
-                    <!--<div class="club-nav">-->
-                        {{--<div class="uk-card-badge">
-                            <ul class="uk-navbar-nav uk-iconnav" uk-navbar="mode: click; boundary-align: true; align: center;">
-                                <li>
-                                    <a href="#" class="uk-icon-button" uk-icon="icon: more-vertical"></a>
-                                    <div class="uk-navbar-dropdown">
-                                        <ul class="uk-nav uk-navbar-dropdown-nav">
-                                            <li><a href="#"><span uk-icon="icon: plus-circle"></span>Apply for membership</a></li>
-                                            <li class="uk-nav-divider"></li>
-                                            <li>
-                                                <a href="{{route('clubManagement.edit', $club->id)}}"><span uk-icon="icon: pencil"></span>Edit club</a>
-                                            </li>
-                                            <li>
-                                                <button id="delete" data-id="{{$club->id}}" onclick="myFunction()">
-                                                    <span uk-icon="icon: minus-circle"></span>Delete club
-                                                </button>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </li>
-                            </ul>
-                        </nav>--}}
                     </div>
 
 
                 @include('clubs.ekstra.members')
+                <div class="primary-btn primary-btn--center">
+                    <a href="#">View more</a>
+                </div>
 
                 <!-- CLUB WINE COLLECTION -->
                 <div class="club-profile-collection">
